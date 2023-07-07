46bea09fefc6bb2cde5361f0b455302e88b27b6c
Stelusno <stelusno@gmail.com>
2023-07-06 21:40:16 -0400

    Full game complete

--- game.py
+++ game.py
@@ -40,20 +40,59 @@ while True:
 
 
 #Input option on whether to Cut Grass or Buy Tool
-  user_choice = input("Enter Option: 'Cut Grass' or 'Buy Tool':")
-
-#Conditionals for options that are chosen
-  if user_choice == "Cut Grass" and current_tool['name'] == "Teeth":
-    current_money += 1
-  elif user_choice == "Buy Tool" and current_tool['name'] == "Teeth" and current_money < 5:
-    print("Not Enough Money!")
-  # elif user_choice == "Cut Grass" and current_tool['name'] == "Teeth":
-  #   current_money += 1
-
-#Quit game 
+  user_choice = input("Enter Option: 'Cut Grass' or 'Buy Tool':").lower()
+#Quit game conditional 
   if user_choice.lower() == "quit":
     print("Exting the game. Goodbye!")
     break 
+  print(f"You entered {user_choice}.")
+  if user_choice.lower() not in ('cut grass', 'buy tool'):
+    print("Not A Valid Option")
+
+#Conditionals for options that are chosen
+#Cut Grass Conditionals
+  if user_choice == "cut grass" and current_tool['name'] == "Teeth":
+    current_money += 1
+  elif user_choice == "cut grass" and current_tool['name'] == "Rusty Scissors":
+    current_money += 5
+  elif user_choice == "cut grass" and current_tool['name'] == "Old Push Mower":
+    current_money += 50
+  elif user_choice == "cut grass" and current_tool['name'] == "Battery Powered Lawnmower":
+    current_money += 100
+  elif user_choice == "cut grass" and current_tool['name'] == "Starving Students":
+    current_money += 250
+#Buy Tool Conditionals
+  elif user_choice == "buy tool" and current_tool['name'] == "Teeth" and current_money < 5:
+    print("Not Enough Money! Keep Cutting!")
+  elif user_choice == "buy tool" and current_tool['name'] == "Rusty Scissors" and current_money < 25:
+    print("Not Enough Money! Keep Cutting!")
+  elif user_choice == "buy tool" and current_tool['name'] == "Old Push Mower" and current_money < 250:
+    print("Not Enough Money! Keep Cutting!" )
+  elif user_choice == "buy tool" and current_tool['name'] == "Battery Powered Lawnmower" and current_money < 500:
+    print("Not Enough Money! Keep Cutting!")
+#Switch Tool Conditionals
+  elif user_choice == "buy tool" and current_tool['name'] == "Teeth" and current_money >= 5:
+    current_tool = tools.pop(0)
+    current_money -= 5
+  elif user_choice == "buy tool" and current_tool['name'] == "Rusty Scissors" and current_money >= 25: 
+    current_tool = tools.pop(0)
+    current_money -= 25
+  elif user_choice == "buy tool" and current_tool['name'] == "Old Push Mower" and current_money >= 250: 
+    current_tool = tools.pop(0)
+    current_money -= 250
+  elif user_choice == "buy tool" and current_tool['name'] == "Battery Powered Lawnmower" and current_money >= 500: 
+    current_tool = tools.pop(0)
+    current_money -= 500
+
+
   
-  print(f"The user entered {user_choice}. Current Money:           {current_money}")
+
+  
+  
+  
+#Win Game Condition
+  if current_money >= 1000 and current_tool['name'] == "Starving Students" :
+    print("You Win!! Game Over!!")
+    break 
+    
   
